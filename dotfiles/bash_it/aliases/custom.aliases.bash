ebb27d9472bf784bbf19036fc87a4bb8a7c7e01e
Lorenzo <lorenzo.carbonell.cerezo@gmail.com>
2021-10-22 06:49:12 +0200

    fzf

--- dotfiles/bash_it/aliases/custom.aliases.bash
+++ dotfiles/bash_it/aliases/custom.aliases.bash
@@ -1,7 +1,7 @@
 alias vim=nvim
 alias vimdiff='nvim -d'
 alias j=autojump
-alias dotdrop='eval $(grep -v "^#" /datos/misdotfiles/.env);/datos/misdotfiles/dotdrop.sh --cfg=/datos/misdotfiles/config.yaml'
+alias dotdrop='eval $(grep -v "^#" /datos/misdotfiles/.env);/bin/bash /datos/misdotfiles/dotdrop.sh --cfg=/datos/misdotfiles/config.yaml'
 alias screenkey='/datos/programacion/python/screenkey/screenkey'
 alias sshk='kitty +kitten ssh'
 alias icat="kitty +kitten icat"
