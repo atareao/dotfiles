91d90b0d24c8bb39e88295ba3811f704b3a99084
Lorenzo <lorenzo.carbonell.cerezo@gmail.com>
2021-12-23 06:21:57 +0100

    ohmyzsh

---
+++ dotfiles/oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,10 @@
+alias vim=nvim
+alias vimdiff='nvim -d'
+alias dotdrop='eval $(grep -v "^#" ~/dotfiles/.env);/bin/bash ~/dotfiles/dotdrop.sh --cfg=~/dotfiles/config.yaml'
+alias sshk='kitty +kitten ssh'
+alias icat='kitty +kitten icat'
+alias mydiff='kitty +kitten diff'
+alias ls=exa
+alias cat=bat
+alias du=dust
+alias find=fd
---
+++ dotfiles/oh-my-zsh/custom/plugins/atareao/atareao.plugin.zsh
@@ -0,0 +1,10 @@
+SCRIPTDIR="${0:h}"
+
+source ${SCRIPTDIR}/iconify.sh
+source ${SCRIPTDIR}/ignore.sh
+source ${SCRIPTDIR}/imp3.sh
+source ${SCRIPTDIR}/minify.sh
+source ${SCRIPTDIR}/rust2ppa.sh
+source ${SCRIPTDIR}/mybuilder.sh
+source ${SCRIPTDIR}/toclip.sh
+source ${SCRIPTDIR}/helper_python.sh
