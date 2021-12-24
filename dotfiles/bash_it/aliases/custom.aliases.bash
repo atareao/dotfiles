fb80fa063ccfbe3c668f0945a6a77fafd96136fd
Lorenzo <lorenzo.carbonell.cerezo@gmail.com>
2021-12-24 20:13:47 +0100

    clean bash

--- dotfiles/bash_it/aliases/custom.aliases.bash
+++
@@ -1,8 +0,0 @@
-alias vim=nvim
-alias vimdiff='nvim -d'
-alias j=autojump
-alias dotdrop='eval $(grep -v "^#" {{@@ dotfilesdir @@}}/.env);/bin/bash {{@@ dotfilesdir @@}}/dotdrop.sh --cfg={{@@ dotfilesdir @@}}/config.yaml'
-alias screenkey='/datos/programacion/python/screenkey/screenkey'
-alias sshk='kitty +kitten ssh'
-alias icat="kitty +kitten icat"
-alias mydiff="kitty +kitten diff"
--- dotfiles/bash_it/lib/custom.bash
+++
@@ -1,8 +0,0 @@
-SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-source ${SCRIPTDIR}/iconify.sh
-source ${SCRIPTDIR}/ignore.sh
-source ${SCRIPTDIR}/imp3.sh
-source ${SCRIPTDIR}/minify.sh
-source ${SCRIPTDIR}/rust2ppa.sh
-source ${SCRIPTDIR}/mybuilder.sh
