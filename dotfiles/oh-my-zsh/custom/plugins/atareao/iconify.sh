caf1a8fa4bb852cc3ffe64db38ad8e02dc184f04
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-04-11 08:06:57 +0200

    obs update

--- dotfiles/oh-my-zsh/custom/plugins/atareao/iconify.sh
+++ dotfiles/oh-my-zsh/custom/plugins/atareao/iconify.sh
@@ -2,11 +2,11 @@
 
 function iconify(){
     filename="$1"
-    if [ -f "${filename}" ]
+    if [[ -f "${filename}" ]]
     then
         output="${filename/.png}.webp"
         cwebp "${filename}" -o "${output}"
-        salida=$(cat "${output}" | base64 | tr -d '\n')
+        salida=$(base64 < "${output}" | tr -d '\n')
         echo "data:image/webp;base64,${salida}"
     fi
 }
