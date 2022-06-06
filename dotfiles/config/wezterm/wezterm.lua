2c291d8ebaaa2f96761191aaf3c804a4e9641411
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-06-06 20:55:06 +0200

    wezterm

---
+++ dotfiles/config/wezterm/wezterm.lua
@@ -0,0 +1,27 @@
+local wezterm = require 'wezterm';
+return {
+    enable_wayland = true,
+    font = wezterm.font({
+        family="JetBrains Mono",
+        harfbuzz_features={"calt=1", "clig=1", "liga=1"}
+    }),
+    font_size = 13,
+    color_scheme = "ayu",
+    use_fancy_tab_bar = false,
+    enable_tab_bar = true,
+    hide_tab_bar_if_only_one_tab = true,
+    keys = {
+        {key="c", mods="CTRL|ALT", action=wezterm.action{CopyTo="ClipboardAndPrimarySelection"}},
+        {key="v", mods="CTRL|ALT", action="Paste"}
+    },
+    mouse_bindings = {
+        {event={Up={streak=1, button="Right"}}, mods="NONE", action=wezterm.action{PasteFrom="PrimarySelection"}},
+    },
+    ssh_domains = {
+        {
+            name = "gk55",
+            remote_address = "gk55",
+            username = "lorenzo"
+        }
+    }
+}
