16d5600eaa9c661e1a9fb250ec8d01d6df877743
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-06-20 18:25:52 +0200

    updated nvim

--- dotfiles/config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dotfiles/config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -97,7 +97,7 @@ https://github.com/typescript-language-server/typescript-language-server
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'bashls', 'pyright', 'clangd', 'html'}--, 'tsserver'
+local servers = { 'bashls', 'pyright', 'clangd', 'html', 'tsserver'}--, 'tsserver'
 
 -- Set settings for language servers below
 --
