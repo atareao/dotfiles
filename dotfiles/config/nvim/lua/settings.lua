39f67cb2e491815e0815972178bc88319a64ee07
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-06-29 07:24:15 +0200

    templates for javascript and rust

--- dotfiles/config/nvim/lua/settings.lua
+++ dotfiles/config/nvim/lua/settings.lua
@@ -5,12 +5,12 @@
 -----------------------------------------------------------
 -- Neovim API aliases
 -----------------------------------------------------------
---local map = vim.api.nvim_set_keymap  -- set global keymap
-local cmd = vim.cmd     				-- execute Vim commands
-local exec = vim.api.nvim_exec 	-- execute Vimscript
-local fn = vim.fn       				-- call Vim functions
-local g = vim.g         				-- global variables
-local opt = vim.opt         		-- global/buffer/windows-scoped options
+--local map = vim.api.nvim_set_keymap -- set global keymap
+local cmd = vim.cmd                   -- execute Vim commands
+local exec = vim.api.nvim_exec        -- execute Vimscript
+local fn = vim.fn                     -- call Vim functions
+local g = vim.g                       -- global variables
+local opt = vim.opt                   -- global/buffer/windows-scoped options
 
 -----------------------------------------------------------
 -- General
