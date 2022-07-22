1da1b6907b0a12d23baf221d095325ba2f1f205a
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-07-22 21:02:22 +0200

    eww

--- dotfiles/config/eww/launch.sh
+++ dotfiles/config/eww/launch.sh
@@ -21,4 +21,30 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}' | tr -d '%'
+#!/bin/bash
+
+## Files and cmd
+FILE="$HOME/.cache/eww_launch.xyz"
+EWW="$HOME/.local/bin/eww -c $HOME/.config/eww/bar"
+
+## Run eww daemon if not running already
+if [[ ! $(pidof eww) ]]; then
+    ${EWW} daemon
+    sleep 1
+fi
+
+## Open widgets
+run_eww() {
+    ${EWW} open-many \
+         bar
+
+}
+
+## Launch or close widgets accordingly
+if [[ ! -f "$FILE" ]]; then
+    touch "$FILE"
+    run_eww
+else
+    ${EWW} close-all && killall eww
+    rm "$FILE"
+fi
