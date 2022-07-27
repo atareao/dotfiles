78ef644f598dacacba608ac7a854586015192032
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-07-27 21:23:05 +0200

    imp3 new location

--- dotfiles/oh-my-zsh/custom/plugins/atareao/imp3.sh
+++ dotfiles/oh-my-zsh/custom/plugins/atareao/imp3.sh
@@ -1,12 +1,54 @@
 #!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2022 Lorenzo Carbonell <a.k.a. atareao>
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 function imp3(){
-    JPG_FILE="$(find . -type f -name '*.jpg' | head -n1)"
-    MP3_FILE="$(find . -type f -name '*.mp3' | head -n1)"
-    MD_FILE="$(find . -type f -name '*.md' | head -n1)"
-    echo "$JPG_FILE"
+    EPISODE="$1"
+    if [[ -z "$EPISODE" ]];then
+        echo "Introduce el número del episodio"
+        exit 1
+    fi
+    NEED_EXIT=0
+    MP3_FILE="/data/podcasts/audio/e${EPISODE}.mp3"
+    if [[ ! -f "${MP3_FILE}" ]];then
+        echo "No existe el archivo ${MP3_FILE}"
+        NEED_EXIT=1
+    fi
+    JPG_FILE="/data/podcasts/image/e${EPISODE}.jpg"
+    if [[ ! -f "${JPG_FILE}" ]];then
+        echo "No existe el archivo ${JPG_FILE}"
+        NEED_EXIT=1
+    fi
+    MD_FILE="/data/notas/e${EPISODE}.md"
+    if [[ ! -f "${MD_FILE}" ]];then
+        echo "No existe el archivo ${MD_FILE}"
+        NEED_EXIT=1
+    fi
+    if [[ $NEED_EXIT -eq 1 ]];then
+        exit 1
+    fi
     echo "$MP3_FILE"
-    echo "${MP3_FILE/.mp3}_original.mp3"
+    echo "$JPG_FILE"
+    echo "$MD_FILE"
     eyeD3 --add-image "$JPG_FILE":FRONT_COVER "$MP3_FILE"
     eyeD3 -P itunes-podcast --add "$MP3_FILE"
     eyeD3 -P itunes-podcast "$MP3_FILE"
