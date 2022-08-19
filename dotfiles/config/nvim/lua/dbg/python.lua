c9962830b2d1265e269ed4832acf90188bab42b2
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-08-19 19:19:10 +0200

    debut python

---
+++ dotfiles/config/nvim/lua/dbg/python.lua
@@ -0,0 +1,19 @@
+local dap = require("dap")
+dap.adapters.python = {
+    type = "executable",
+    command = vim.fn.getcwd() .. '/bin/python',
+    args = { "-m", "debugpy.adapter"},
+}
+dap.configurations.python = {
+    {
+        name = "Launch",
+        type = "python",
+        request = "launch",
+        program = function()
+            return vim.fn.input('Ruta al main: ', vim.fn.getcwd() .. '/src/', 'file')
+        end,
+        cwd = '${workspaceFolder}',
+        stopOnEntry = true,
+        args = {},
+    }
+}
