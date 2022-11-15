dfa9c3b78205d2d7f8086aad473bf0888d6fb3e9
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-11-15 05:17:12 +0100

    remove sj

--- dotfiles/config/nvim/lua/plugins/sj.lua
+++
@@ -1,7 +0,0 @@
-local sj = require("sj")
-sj.setup()
-
-vim.keymap.set("n", "<leader>sj", sj.run)
-vim.keymap.set("n", "<A-,>", sj.prev_match)
-vim.keymap.set("n", "<A-;>", sj.next_match)
-vim.keymap.set("n", "<localleader>s", sj.redo)
