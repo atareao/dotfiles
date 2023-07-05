5de8e93cdbadecb5a2f55e715d01bdfc4a503308
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2023-07-05 06:47:34 +0200

    keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -12,67 +12,30 @@ local cmd = vim.cmd
 -- Neovim shortcuts:
 -----------------------------------------------------------
 
--- clear search highlighting
-map('n', '<leader>c', ':nohl<CR>', default_opts)
--- reload configuracion
-map('n', '<leader>r', ':source % <CR>', default_opts)
-
--- don't use arrow keys
-map('', '<up>', '<nop>', {noremap = true})
-map('', '<down>', '<nop>', {noremap = true})
-map('', '<left>', '<nop>', {noremap = true})
-map('', '<right>', '<nop>', {noremap = true})
-
-map('i', '<C-h>', '<left>', default_opts)
-map('i', '<C-j>', '<down>', default_opts)
-map('i', '<C-k>', '<up>', default_opts)
-map('i', '<C-l>', '<right>', default_opts)
-
--- fast saving with <leader> and s
-map('n', '<leader>s', ':w<CR>', default_opts)
-map('i', '<leader>s', '<C-c>:w<CR>', default_opts)
 
 -- move around splits using Ctrl + {h,j,k,l}
-map('n', '<C-h>', '<C-w>h', default_opts)
-map('n', '<C-j>', '<C-w>j', default_opts)
-map('n', '<C-k>', '<C-w>k', default_opts)
-map('n', '<C-l>', '<C-w>l', default_opts)
+-- map('n', '<C-h>', '<C-w>h', default_opts)
+-- map('n', '<C-j>', '<C-w>j', default_opts)
+-- map('n', '<C-k>', '<C-w>k', default_opts)
+-- map('n', '<C-l>', '<C-w>l', default_opts)
 
--- close all windows and exit from neovim
-map('n', '<leader>q', ':quitall<CR>', default_opts)
 
 -----------------------------------------------------------
 -- Applications & Plugins shortcuts:
 -----------------------------------------------------------
--- open terminal
-map('n', '<C-t>', ':Term<CR>', {noremap = true})
-map('t', '<C-w>h', '<C-\\><C-n><C-w>h', {noremap = true})
-map('t', '<C-w>j', '<C-\\><C-n><C-w>j', {noremap = true})
-map('t', '<C-w>k', '<C-\\><C-n><C-w>k', {noremap = true})
-map('t', '<C-w>l', '<C-\\><C-n><C-w>l', {noremap = true})
-map('t', '<C-w><C-w>', '<C-\\><C-n><C-w><C-w>', {noremap = true})
 
---nvim-lazygit
-map('n', '<C-g>', ':LazyGit<CR>', default_opts)       -- open/close
-
---nvim-fzf-lua
-map('n', '<C-p>', ':FzfLua files<CR>', default_opts)       -- open/close
 
 -- nvim-tree
-map('n', '<C-n>', ':Neotree toggle<CR>', default_opts)       -- open/close
+--map('n', '<C-n>', ':Neotree toggle<CR>', default_opts)       -- open/close
 --map('n', '<leader>r', ':NvimTreeRefresh<CR>', default_opts)  -- refresh
 --map('n', '<leader>n', ':NvimTreeFindFile<CR>', default_opts) -- search file
 
 -- Vista tag-viewer
-map('n', '<C-q>', ':Vista!!<CR>', default_opts)   -- open/close
+--map('n', '<C-q>', ':Vista!!<CR>', default_opts)   -- open/close
 map('i', '<C-q>', ':Vista!!<CR>', default_opts)   -- open/close
-map('n', '<leader>m', ':Vista!!<CR>', default_opts)
-
-map('n', '<leader>tf', ':Telescope file_browser<CR>', default_opts)
-map('n', '<leader>ts', ':Telescope symbols<CR>', default_opts)
 
 -- on hesitation, bring up the panel
-map('n', '<C-s>', ':SidebarNvimToggle<CR>', default_opts)
+--map('n', '<C-s>', ':SidebarNvimToggle<CR>', default_opts)
 
 -- Trouble
 
@@ -114,11 +77,6 @@ for i = 1,9 do
   map('n', ('<Leader>%s'):format(i), ('<Plug>(cokeline-focus-%s)'):format(i), { silent = true })
 end
 
--- Neogen
-map("n", "<leader>ng", ":lua require('neogen').generate()<CR>", { noremap = true })
-
--- ssr
-vim.keymap.set({"n", "x"}, "<leader>sr", function() require("ssr").open() end)
 
 -- noterm
 local NOREF_NOERR_TRUNC = { noremap = true, silent = true, nowait = true }
@@ -130,6 +88,4 @@ map('t', '<M-Tab>', function () vim.cmd('NeoTermClose') end, NOREF_NOERR_TRUNC)
 map('t', '<C-w>', function () vim.cmd('NeoTermEnterNormal') end, NOREF_NOERR_TRUNC)
 map('t', '<M-w>', function () vim.cmd('NeoTermEnterNormal') end, NOREF_NOERR_TRUNC)
 -- emoji
-map("n", "<Leader>ei", "<cmd>IconPickerNormal<cr>", opts)
-map("n", "<Leader>ey", "<cmd>IconPickerYank<cr>", opts) --> Yank the selected icon into register
 map("i", "<C-e>", "<cmd>IconPickerInsert<cr>", opts)
