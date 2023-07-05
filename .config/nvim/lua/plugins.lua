6b50bd4cd2a262e6f232e9b846407230afa3bf38
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2023-07-05 06:53:13 +0200

    update whichkey

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -291,6 +291,11 @@ require("lazy").setup({
     -- Whichkey
     {
         "folke/which-key.nvim",
+        event = "VeryLazy",
+        init = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+        end,
         config = function()
             require("config.whichkey")
         end
