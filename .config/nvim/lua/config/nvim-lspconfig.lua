5f4d69a46f2e1cd14ff125dfabe6e7bff0302da3
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2023-08-19 09:38:49 +0200

    emojis in nvim

--- .config/nvim/lua/config/nvim-lspconfig.lua
+++ .config/nvim/lua/config/nvim-lspconfig.lua
@@ -60,7 +60,7 @@ end
 --     signs = true,
 --     float = { border = "single"},
 -- })
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = "☢️ ", Warn = "⚠️ ", Hint = "? ", Info = "ℹ️ " }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
--- .config/nvim/lua/config/trouble.lua
+++ .config/nvim/lua/config/trouble.lua
@@ -37,11 +37,11 @@ require('trouble').setup({
     auto_jump = {"lsp_definitions"}, -- for the given modes, automatically jump if there is only a single result
     signs = {
         -- icons / text used for a diagnostic
-        error = "",
-        warning = "",
-        hint = "",
-        information = "",
-        other = "﫠"
+        error = "☢️",
+        warn = "⚠️",
+        hint = "🔍",
+        info = "ℹ️",
+        other = "🤷"
     },
     use_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
 })
