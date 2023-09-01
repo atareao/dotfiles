6b3b1dc4d070efc1d075d7f6995a104f3d4ad352
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2023-09-01 06:41:27 +0200

    backup

--- .local/bin/launch_backup.sh
+++ .local/bin/launch_backup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-while read -r item;do
-    variable="${item/\//_}"
-    echo "${variable}"
-    export "${variable}=$(gopass show --password "$item")"
-done < <(gopass list --flat)
-yadm alt
-
-/bin/bash "$HOME/lorenzo/.local/bin/backup.sh"
+# while read -r item;do
+#     variable="${item/\//_}"
+#     echo "${variable}"
+#     export "${variable}=$(gopass show --password "$item")"
+# done < <(gopass list --flat)
+# yadm alt
+# 
+# /bin/bash "$HOME/.local/bin/backup.sh"
+/bin/bash "$HOME/.local/bin/backup.sh"
