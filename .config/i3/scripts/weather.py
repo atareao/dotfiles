ca10064cd0f8ff9b6e610c23692e5a75ad9cb295
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2023-09-16 06:10:24 +0200

    weather

--- .config/i3/scripts/weather.py
+++ .config/i3/scripts/weather.py
@@ -26,7 +26,7 @@ import sys
 import logging
 
 logging.getLogger(__name__)
-logging.basicConfig(level=logging.ERROR)
+logging.basicConfig(level=logging.DEBUG)
 
 BASE_URL = "https://api.open-meteo.com"
 
@@ -38,6 +38,24 @@ class OpenMeteoClient:
 
     @staticmethod
     def get_icon(code: int) -> str:
+        """
+        WMO Weather interpretation codes (WW)
+        Code        Description
+        0           Clear sky
+        1, 2, 3     Mainly clear, partly cloudy, and overcast
+        45, 48      Fog and depositing rime fog
+        51, 53, 55  Drizzle: Light, moderate, and dense intensity
+        56, 57      Freezing Drizzle: Light and dense intensity
+        61, 63, 65  Rain: Slight, moderate and heavy intensity
+        66, 67      Freezing Rain: Light and heavy intensity
+        71, 73, 75  Snow fall: Slight, moderate, and heavy intensity
+        77          Snow grains
+        80, 81, 82  Rain showers: Slight, moderate, and violent
+        85, 86      Snow showers slight and heavy
+        95 *        Thunderstorm: Slight or moderate
+        96, 99 *    Thunderstorm with slight and heavy hail
+        (*) Thunderstorm forecast with hail is only available in Central Europe
+        """
         if code == 0:
             # Clear sky
             return "☀️"
@@ -66,8 +84,7 @@ class OpenMeteoClient:
             # 77 Snow grains
             pass
         elif code in [80, 81, 82]:
-            # Rain showers: Slight, moderate, and violent
-            pass
+            return "🌧️"
         elif code in [85, 86]:
             # Snow showers slight and heavy
             pass
@@ -109,3 +126,9 @@ if __name__ == "__main__":
         longitude = float(sys.argv[2])
         client = OpenMeteoClient(latitude, longitude)
         print(client.current_weather())
+    else:
+        latitude = 39.36667
+        longitude = -0.41667
+        client = OpenMeteoClient(latitude, longitude)
+        print(client.current_weather())
+
