66cc76b9208b7a94740de36cbfcfdbf19e88b96a
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2023-09-29 09:14:57 +0200

    himalaya

--- .config/nvim/lua/config/indent-blankline.lua
+++ .config/nvim/lua/config/indent-blankline.lua
@@ -8,7 +8,7 @@
 
 vim.opt.list = true
 vim.opt.listchars:append("space:⋅")
-require('indent_blankline').setup {
+require('ibl').setup {
   char = "▏",
   space_char_blankline = " ",
   show_first_indent_level = false,
