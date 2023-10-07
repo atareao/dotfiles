fee5350067ec59bb67751a40e0ab8917b7bb7fa3
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2023-10-07 07:41:30 +0200

    null-ls

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -24,10 +24,11 @@ return {
 
         local config = {
             -- enable virtual text
-            virtual_text = true,
-            update_in_insert = true,
+            virtual_text = false,
+            signs = true,
+            update_in_insert = false,
             underline = true,
-            severity_sort = true,
+            severity_sort = false,
             float = {
                 focusable = true,
                 style = "minimal",
@@ -38,16 +39,54 @@ return {
                 -- width = 40,
             },
         }
+        vim.o.updatetime = 250
+        vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
 
-        local lspconfig = require("lspconfig")
-        lspconfig.pylyzer.setup({
-            settings = {
-                pylyzer = {
-                    diagnostics = false,
-                    inlayHints = true
-                }
-            }
-        })
+        local has_words_before = function()
+            local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+            return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+        end
+
+        local feedkey = function(key, mode)
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+        end
+
+        local on_attach = function(client, bufnr)
+            local bufopts = { noremap=true, silent=true, buffer=bufnr }
+            vim.keymap.set('n', '<leader>d', vim.lsp.buf.definition, bufopts)
+        end
+
+        -- Pyright
+        local configs = require('lspconfig/configs')
+        local util = require('lspconfig/util')
+
+        local path = util.path
+        local function get_python_path(workspace)
+            -- Use activated virtualenv.
+            if vim.env.VIRTUAL_ENV then
+                return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
+            end
+
+            -- Find and use virtualenv in workspace directory.
+            for _, pattern in ipairs({'*', '.*'}) do
+                local match = vim.fn.glob(path.join(workspace, pattern, 'pyvenv.cfg'))
+                if match ~= '' then
+                    return path.join(path.dirname(match), 'bin', 'python')
+                end
+            end
+
+            -- Fallback to system Python.
+            return exepath('python3') or exepath('python') or 'python'
+        end
+
+        require("lspconfig").pyright.setup{
+            before_init = function(_, config)
+                config.settings.python.pythonPath = get_python_path(config.root_dir)
+            end,
+            on_attach = on_attach,
+            capabilities = capabilities,
+        }
+
 
         vim.diagnostic.config(config)
         require("inlay-hints").setup()
@@ -62,7 +101,7 @@ return {
             -- auto-install configured servers (with lspconfig)
             automatic_installation = true, -- not the same as ensure_installed
         })
-        
+
         local servers = {
             efm = {},
             pyright = {},
@@ -84,26 +123,26 @@ return {
             end,
         }
         --------------------------------------------------------------------------------
--- activate each lsp in the list
---------------------------------------------------------------------------------
-for _, lsp in ipairs(servers) do
-    local generic_opts = {
-       capabilities = capabilities,
-       on_attach = handlers.on_attach,
-       flags = { debounce_text_changes = 150 },
-    }
-    local specialized_opts = option_lookup[lsp]
-    if specialized_opts == nil then
-       lspconfig[lsp].setup(generic_opts)
-    else
-       local opts = vim.tbl_deep_extend(
-           "force",
-           generic_opts,
-           specialized_opts
-        )
-        lspconfig[lsp].setup(opts)
-    end
-end
+        -- activate each lsp in the list
+        --------------------------------------------------------------------------------
+        for _, lsp in ipairs(servers) do
+            local generic_opts = {
+                capabilities = capabilities,
+                on_attach = handlers.on_attach,
+                flags = { debounce_text_changes = 150 },
+            }
+            local specialized_opts = option_lookup[lsp]
+            if specialized_opts == nil then
+                lspconfig[lsp].setup(generic_opts)
+            else
+                local opts = vim.tbl_deep_extend(
+                    "force",
+                    generic_opts,
+                    specialized_opts
+                )
+                lspconfig[lsp].setup(opts)
+            end
+        end
     end,
 }
 
--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -3,10 +3,38 @@ return {
     event = "VeryLazy",
     config = function()
         local null_ls = require("null-ls")
+        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
         null_ls.setup({
-            null_ls.builtins.formatting.shfmt, -- shell script formatting
-            null_ls.builtins.formatting.prettier, -- markdown formatting
-            null_ls.builtins.diagnostics.shellcheck, -- shell script diagnostics
+            sources = {
+                -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#diagnostics
+                -- formatting
+                null_ls.builtins.formatting.ruff,
+                null_ls.builtins.formatting.shfmt, -- shell script formatting
+                null_ls.builtins.formatting.prettier, -- markdown formatting
+                -- diagnostics
+                null_ls.builtins.diagnostics.shellcheck, -- shell script diagnostics
+                null_ls.builtins.diagnostics.flake8, -- shell script diagnostics
+                null_ls.builtins.diagnostics.ruff, -- shell script diagnostics
+                null_ls.builtins.diagnostics.hadolint, -- docker diagnostics
+                null_ls.builtins.diagnostics.jasolint, -- javascript diagnostics
+                null_ls.builtins.diagnostics.markdownlint, -- markdown diagnostics
+                null_ls.builtins.diagnostics.pycodestyle, -- markdown diagnostics
+                null_ls.builtins.diagnostics.pydocstyle, -- markdown diagnostics
+                -- completions
+                null_ls.builtins.completion.spell,
+            },
+            on_attach = function(client, bufnr)
+                if client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = augroup,
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format({ bufnr = bufnr })
+                        end,
+                    })
+                end
+            end,
         })
     end
 }
