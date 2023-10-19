c9645e450e8c0012520949651a407bca7ca626b6
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2023-10-19 07:02:02 +0200

    ZettelKasten

--- .config/nvim/lua/plugins/zk.lua
+++ .config/nvim/lua/plugins/zk.lua
@@ -1,11 +1,13 @@
+-- Plugin from:
+-- https://github.com/mickael-menu/zk-nvim
 return {
     "mickael-menu/zk-nvim",
     config = function()
-        require("zk").setup({
+        local zk = require("zk")
+        zk.setup({
             -- can be "telescope", "fzf" or "select" (`vim.ui.select`)
             -- it's recommended to use "telescope" or "fzf"
             picker = "telescope",
-
             lsp = {
                 -- `config` is passed to `vim.lsp.start_client(config)`
                 config = {
@@ -14,7 +16,6 @@ return {
                     -- on_attach = ...
                     -- etc, see `:h vim.lsp.start_client()`
                 },
-
                 -- automatically attach buffers in a zk notebook that match the given filetypes
                 auto_attach = {
                     enabled = true,
@@ -22,5 +23,10 @@ return {
                 },
             },
         })
+        local commands = require("zk.commands")
+        commands.add("ZkOrphans", function(options)
+            options = vim.tbl_extend("force", { orphan = true }, options or {})
+            zk.edit(options, { title = "Zk Orphans" })
+        end)
     end
 }
