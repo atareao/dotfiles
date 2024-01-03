03e171c5be4104c6503e3bbc82df34202f1c214b
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2024-01-03 09:18:31 +0100

    typescript

---
+++ .config/zsh/plugins/atareao/yazi.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+function ya() {
+    tmp="$(mktemp -t "yazi-cwd.XXXXX")"
+    yazi "$@" --cwd-file="$tmp"
+    if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+        cd -- "$cwd"
+    fi
+    rm -f -- "$tmp"
+}
