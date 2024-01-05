7b12fba7713e42615be8e1d7c6e0fab5ce100eb5
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2024-01-05 19:58:10 +0100

    Reformat

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -13,7 +13,7 @@ return {
             sh = { "shfmt" },
             bash = { "shfmt" },
         })
-        vim.api.nvim_create_user_command("Format", function(args)
+        vim.api.nvim_create_user_command("Reformat", function(args)
             local range = nil
             if args.count ~= -1 then
                 local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
