59d1f646c34f74c7acceef7dd07858cd4887bd20
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2024-01-14 12:23:02 +0100

    fish and ayu

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,11 @@
-
 fish_add_path -g ~/.cargo/bin ~/.local/bin ~/go/bin/ ~/bin /usr/local/bin
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
     # remove welcome message
     set -g fish_greeting
+    # Theme
+    fish_config theme choose "ayu Mirage"
     # starship
     starship init fish | source
     # atuin
