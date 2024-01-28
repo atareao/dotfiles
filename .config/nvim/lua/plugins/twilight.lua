961eec62dcb3a864ad03923ac8cf4072f7fc92d8
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2024-01-28 09:23:22 +0100

    zen-mode

---
+++ .config/nvim/lua/plugins/twilight.lua
@@ -0,0 +1,9 @@
+return -- Lua
+{
+    "folke/twilight.nvim",
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+    }
+}
---
+++ .config/nvim/lua/plugins/zen-mode.lua
@@ -0,0 +1,13 @@
+return -- Lua
+{
+    "folke/zen-mode.nvim",
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+        kitty = {
+            enabled = true,
+        }
+    }
+}
+
