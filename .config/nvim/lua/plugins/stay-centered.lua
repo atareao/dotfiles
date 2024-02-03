34cfece8e9358480cd4169801e3c108386ca3434
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2024-02-03 09:27:42 +0100

    autopairs

---
+++ .config/nvim/lua/plugins/stay-centered.lua
@@ -0,0 +1,13 @@
+return {
+    "arnamak/stay-centered.nvim",
+    opts = {
+        skip_filetypes = {
+            "lua",
+            "typescript",
+            "python",
+            "rust",
+            "toml"
+        },
+    },
+    config = true
+}
