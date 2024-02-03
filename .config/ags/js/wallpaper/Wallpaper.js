eda9fc7f99442f3931f9b1cf6386427844028ec5
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2024-02-03 10:18:40 +0100

    corners

--- .config/ags/js/wallpaper/Wallpaper.js
+++ .config/ags/js/wallpaper/Wallpaper.js
@@ -1,9 +1,11 @@
 import Widget from "resource:///com/github/Aylur/ags/widget.js";
 import cairo from "cairo";
 import Gdk from "gi://Gdk";
+import Gtk from 'gi://Gtk';
 import GdkPixbuf from "gi://GdkPixbuf";
 
 import options from "../options.js";
+
 
 /** @param image  */
 const Wallpaper = (filename) =>
@@ -20,6 +22,10 @@ const Wallpaper = (filename) =>
         const geometry = monitor.get_geometry();
         const width = geometry.width;
         const height = geometry.height;
+        const context = self.get_style_context();
+        const c = context.get_property('background-color', Gtk.StateFlags.NORMAL);
+        //const r = context.get_property('border-radius', Gtk.StateFlags.NORMAL);
+        const r = options.radii.value * 2;
 
         const surface = new cairo.ImageSurface(cairo.FORMAT_ARGB32, width, height);
         const pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale(filename, width, height, false);
@@ -31,6 +37,30 @@ const Wallpaper = (filename) =>
         cr.setSourceSurface(surface, 0, 0);
         cr.paint();
         self.set_size_request(width, height);
+
+        cr.arc(r, r + 20, r, Math.PI, 3 * Math.PI / 2);
+        cr.lineTo(0, 20);
+        cr.closePath();
+        cr.setSourceRGBA(0.07, 0.07, 0.07, 1);
+        cr.fill();
+
+        cr.arc(width - r, r + 20, r, 3 * Math.PI / 2, 2 * Math.PI);
+        cr.lineTo(width, 20);
+        cr.closePath();
+        cr.setSourceRGBA(0.07, 0.07, 0.07, 1);
+        cr.fill();
+
+        cr.arc(width - r, height - r - 20, r, 0, Math.PI / 2);
+        cr.lineTo(width, height);
+        cr.closePath();
+        cr.setSourceRGBA(0.07, 0.07, 0.07, 1);
+        cr.fill();
+
+        cr.arc(r, height - r - 20, r, Math.PI / 2, Math.PI);
+        cr.lineTo(0, height);
+        cr.closePath();
+        cr.setSourceRGBA(0.07, 0.07, 0.07, 1);
+        cr.fill();
       }),
   });
 
