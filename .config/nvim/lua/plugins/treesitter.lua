457995ccf332b20a1e9832d714cb62e9f82fd865
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2024-02-06 06:38:22 +0100

    treesitter config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -6,40 +6,44 @@
 --- https://github.com/nvim-treesitter/nvim-treesitter
 return {
     "nvim-treesitter/nvim-treesitter",
-    lazy = true,
-    opts = {
-        ensure_installed = {
-            "vim", "regex", "lua", "bash", "markdown", "markdown_inline",
-            "python", "rust", "sql", "php", "javascript", "http", "json",
-            "latex", "commonlisp"
-        },
-        highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = { "php", "markdown", "rust", "python" },
-        },
-        rainbow = {
-            enable = true,
-            -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-            extended_mode = true,     -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-            max_file_lines = nil,     -- Do not enable for files with more than n lines, int
-            -- colors = {}, -- table of hex strings
-            -- termcolors = {} -- table of colour name strings
-        },
-        incremental_selection = {
-            enable = false,
-            keymaps = {
-                init_selection = '<CR>',
-                scope_incremental = '<CR>',
-                node_incremental = '<TAB>',
-                node_decremental = '<S-TAB>',
+    build = ":TSUpdate",
+    config = function()
+        local configs = require("nvim-treesitter.configs")
+        configs.setup({
+            ensure_installed = {
+                "vim", "regex", "lua", "bash", "markdown", "markdown_inline",
+                "python", "rust", "sql", "php", "javascript", "http", "json",
+                "latex", "commonlisp"
             },
-        },
-        indent = {
-            enable = true,
-            disable = { "python" },
-        },
-        tree_docs = {
-            enable = true,
-        }
-    }
+            sync_install = false,
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = { "php", "markdown", "rust", "python" },
+            },
+            rainbow = {
+                enable = true,
+                -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+                extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+                max_file_lines = nil, -- Do not enable for files with more than n lines, int
+                -- colors = {}, -- table of hex strings
+                -- termcolors = {} -- table of colour name strings
+            },
+            incremental_selection = {
+                enable = false,
+                keymaps = {
+                    init_selection = '<CR>',
+                    scope_incremental = '<CR>',
+                    node_incremental = '<TAB>',
+                    node_decremental = '<S-TAB>',
+                },
+            },
+            indent = {
+                enable = true,
+                --disable = { "python" },
+            },
+            tree_docs = {
+                enable = true,
+            }
+        })
+    end
 }
