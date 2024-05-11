1b788058f71dff05f900d49aed589a19b32b7de7
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2024-05-11 17:35:02 +0200

    ags

--- .config/ags/config.js
+++ .config/ags/config.js
@@ -1,23 +1,47 @@
-import { readFile } from 'resource:///com/github/Aylur/ags/utils.js';
-import App from 'resource:///com/github/Aylur/ags/app.js';
-import GLib from 'gi://GLib';
-const pkgjson = JSON.parse(readFile(App.configDir + '/package.json'));
+import GLib from "gi://GLib"
 
-const SKIP_CHECK = 'AGS_SKIP_V_CHECK';
+const main = "/tmp/asztal/main.js"
+const entry = `${App.configDir}/main.ts`
+const bundler = GLib.getenv("AGS_BUNDLER") || "bun"
 
 const v = {
-    ags: `v${pkg.version}`,
-    expected: `v${pkgjson.version}`,
-    check: !GLib.getenv(SKIP_CHECK),
-};
-
-function mismatch() {
-    print(`my config expects ${v.expected}, but your ags is ${v.ags}`);
-    print(`to skip the check run "${SKIP_CHECK}=true ags"`);
-    App.connect('config-parsed', app => app.Quit());
-    return {};
+    ags: pkg.version?.split(".").map(Number) || [],
+    expect: [1, 8, 2],
 }
 
-export default v.ags === v.expected || !v.check
-    ? (await import('./js/main.js')).default
-    : mismatch();
+try {
+    switch (bundler) {
+        case "bun": await Utils.execAsync([
+            "bun", "build", entry,
+            "--outfile", main,
+            "--external", "resource://*",
+            "--external", "gi://*",
+            "--external", "file://*",
+        ]); break
+
+        case "esbuild": await Utils.execAsync([
+            "esbuild", "--bundle", entry,
+            "--format=esm",
+            `--outfile=${main}`,
+            "--external:resource://*",
+            "--external:gi://*",
+            "--external:file://*",
+        ]); break
+
+        default:
+            throw `"${bundler}" is not a valid bundler`
+    }
+
+    if (v.ags[1] < v.expect[1] || v.ags[2] < v.expect[2]) {
+        print(`my config needs at least v${v.expect.join(".")}, yours is v${v.ags.join(".")}`)
+        App.quit()
+    }
+
+    await import(`file://${main}`)
+} catch (error) {
+    console.error(error)
+    App.quit()
+}
+
+export { }
+
