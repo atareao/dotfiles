702376a45c0faafb51a61f2a0c419e7729b8d5be
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2024-05-12 11:57:56 +0200

    sway

--- .config/sway/scripts/init_helpers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd i3/i3 || exit
-poetry run python autoname-workspaces.py
-poetry run python autotiling.py
-poetry run python change-to-workspace.py
-poetry run python inactive-windows-transparency.py
-
