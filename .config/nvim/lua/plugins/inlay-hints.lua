7aad97233b58c648207e44f686661a75a3c60df4
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2024-06-14 07:20:17 +0200

    nvim

---
+++ .config/nvim/lua/plugins/inlay-hints.lua
@@ -0,0 +1,8 @@
+return {
+    "MysticalDevil/inlay-hints.nvim",
+    event = "LspAttach",
+    dependencies = { "neovim/nvim-lspconfig" },
+    config = function()
+        require("inlay-hints").setup()
+    end
+}
