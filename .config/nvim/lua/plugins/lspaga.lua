fb8dbb0992f2c0293aa5b1d2181f6163500b9cca
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2024-07-16 10:38:07 +0200

    keybindings

---
+++ .config/nvim/lua/plugins/lspaga.lua
@@ -0,0 +1,8 @@
+return {
+    'nvimdev/lspsaga.nvim',
+    opts = {},
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter', -- optional
+        'nvim-tree/nvim-web-devicons',     -- optional
+    }
+}
