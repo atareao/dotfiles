feb1fa0fb1f522642b7a063e50a576b56824316c
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2024-10-05 18:57:10 +0200

    portada

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -86,7 +86,31 @@ return {
                     -- Any extra CLI arguments for `ruff` go here.
                     args = {},
                 }
-            }
+            },
+            commands = {
+                RuffAutofix = {
+                  function()
+                    vim.lsp.buf.execute_command {
+                      command = 'ruff.applyAutofix',
+                      arguments = {
+                        { uri = vim.uri_from_bufnr(0) },
+                      },
+                    }
+                  end,
+                  description = 'Ruff: Fix all auto-fixable problems',
+                },
+                RuffOrganizeImports = {
+                  function()
+                    vim.lsp.buf.execute_command {
+                      command = 'ruff.applyOrganizeImports',
+                      arguments = {
+                        { uri = vim.uri_from_bufnr(0) },
+                      },
+                    }
+                  end,
+                  description = 'Ruff: Format imports',
+                },
+            },
         }
     end
 }
