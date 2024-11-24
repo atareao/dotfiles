b3077c753d108ffccddc75fa4d6f9dd4660cc29d
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2024-11-24 20:26:15 +0100

    file_browser

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -120,6 +120,7 @@ return {
                 },
             }
         }
+        require("telescope").load_extension("file_browser")
         require("telescope").load_extension("fzy_native")
         require("telescope").load_extension("gitmoji")
         require("telescope").load_extension("emoji")
