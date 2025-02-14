fe895e362ad51cb4d38c9d9241b7bed32906ed6f
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2025-02-14 05:46:45 +0100

    lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,6 +1,20 @@
 local function window()
-  return vim.api.nvim_win_get_number(0)
+    return vim.api.nvim_win_get_number(0)
 end
+
+local clients_lsp = function()
+    local clients = vim.lsp.get_clients()
+    if next(clients) == nil then
+        return ""
+    end
+
+    local c = {}
+    for _, client in pairs(clients) do
+        table.insert(c, client.name)
+    end
+    return " " .. table.concat(c, "|")
+end
+
 local function getWords()
     if vim.fn.mode() == "v" or vim.fn.mode() == "V" or vim.fn.mode() == "" then
         return vim.fn.wordcount().visual_words
@@ -8,15 +22,16 @@ local function getWords()
         return vim.fn.wordcount().words
     end
 end
+
 local function diff_source()
-  local gitsigns = vim.b.gitsigns_status_dict
-  if gitsigns then
-    return {
-      added = gitsigns.added,
-      modified = gitsigns.changed,
-      removed = gitsigns.removed
-    }
-  end
+    local gitsigns = vim.b.gitsigns_status_dict
+    if gitsigns then
+        return {
+            added = gitsigns.added,
+            modified = gitsigns.changed,
+            removed = gitsigns.removed
+        }
+    end
 end
 return {
     'nvim-lualine/lualine.nvim',
@@ -31,18 +46,45 @@ return {
             always_divide_middle = true
         },
         sections = {
-            lualine_a = { {window}, 'mode' },
-            lualine_b = { {'b:gitsigns_head', icon = ''}, {'diff', source = diff_source},
+            lualine_a = {
+                { window },
+                { 'mode', separator = { left = " ", right = "" }, icon = "" },
+            },
+            lualine_b = {
+                {
+                    "filetype",
+                    icon_only = true,
+                    padding = { left = 1, right = 0 },
+                },
+                "filename",
+            },
+            lualine_c = {
+                {
+                    'b:gitsigns_head',
+                    icon = ''
+                },
+                {
+                    'diff',
+                    symbols = { added = " ", modified = " ", removed = " " },
+                    colored = false,
+                    source = diff_source
+                },
                 {
                     'diagnostics',
                     sources = { "nvim_diagnostic" },
-                    symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' }
+                    symbols = { error = " ", warn = " ", info = " ", hint = " " },
+                    update_in_insert = true,
                 }
             },
-            lualine_c = { 'filename' },
             lualine_x = { 'copilot', 'encoding', 'fileformat', 'filetype' },
-            lualine_y = { {getWords}, 'progress' },
-            lualine_z = { 'location' }
+            lualine_y = {
+                { clients_lsp },
+                { getWords },
+                'progress'
+            },
+            lualine_z = {
+                { 'location', separator = { left = "", right = " " }, icon = "" }
+            },
         },
         inactive_sections = {
             lualine_a = {},
@@ -54,8 +96,8 @@ return {
         },
         tabline = {},
         winbar = {},
-        inacvtive_winbar = {},
-        extensions = {}
+        inactive_winbar = {},
+        extensions = { "toggleterm", "trouble" }
     },
     config = true
 }
