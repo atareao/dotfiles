727373d9a33dee875fcb269d0f34a00f3db25dd5
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2025-03-09 08:29:14 +0100

    unjinja

--- .config/fish/functions/unjinja.fish
+++ .config/fish/functions/unjinja.fish
@@ -4,6 +4,7 @@ function unjinja --description="Fill scripts"
     fd -g "*.gpg" $STORE_PATH | while read -l item
         set key $(string replace "$STORE_PATH/" "" $item | path change-extension '')
         set variable $(string upper $key | string replace "/" "_")
+        echo "Setting $variable"
         set -xg $variable "$(pass $key)"
     end
     yadm list -a | while read -l item
