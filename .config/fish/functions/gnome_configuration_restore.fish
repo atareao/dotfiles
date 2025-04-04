4c39de42c72e33fcbaa97c4b7120019ebaec7bc2
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2025-04-04 20:03:32 +0200

    fix

--- .config/fish/functions/gnome_configuration_restore.fish
+++ .config/fish/functions/gnome_configuration_restore.fish
@@ -6,14 +6,14 @@ function gnome_keybind_restore --description="restore a backup of gnome keybinds
         dconf reset -f "/org/gnome/mutter/keybindings/"
         dconf reset -f "/org/gnome/mutter/wayland/keybindings/"
 
-        dconf load "/org/gnome/settings-daemon/plugins/media-keys/" < /.config/gnome/keybindings/media-keys.dconf
-        dconf load "/org/gnome/desktop/wm/keybindings/"             < /.config/gnome/keybindings/wm.dconf
-        dconf load "/org/gnome/shell/keybindings/"                  < /.config/gnome/keybindings/shell.dconf
-        dconf load "/org/gnome/mutter/keybindings/"                 < /.config/gnome/keybindings/mutter.dconf
-        dconf load "/org/gnome/mutter/wayland/keybindings/"         < /.config/gnome/keybindings/mutter-wayland.dconf
+        dconf load "/org/gnome/settings-daemon/plugins/media-keys/" < ~/.config/gnome/keybindings/media-keys.dconf
+        dconf load "/org/gnome/desktop/wm/keybindings/"             < ~/.config/gnome/keybindings/wm.dconf
+        dconf load "/org/gnome/shell/keybindings/"                  < ~/.config/gnome/keybindings/shell.dconf
+        dconf load "/org/gnome/mutter/keybindings/"                 < ~/.config/gnome/keybindings/mutter.dconf
+        dconf load "/org/gnome/mutter/wayland/keybindings/"         < ~/.config/gnome/keybindings/mutter-wayland.dconf
     end
     if [ -d /.config/gnome/extensions ]
         dconf reset -f "/org/gnome/shell/extensions/"
-        dconf load "/org/gnome/shell/extensions/"                   < /.config/gnome/extensions/extensions.dconf
+        dconf load "/org/gnome/shell/extensions/"                   < ~/.config/gnome/extensions/extensions.dconf
     echo "restore done"
 end
