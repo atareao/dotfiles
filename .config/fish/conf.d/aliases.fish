2f8a4eefdce794f8bf24e2bac404cc79eb4e5a3d
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2025-04-25 18:49:26 +0200

    git

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -18,27 +18,27 @@ function dv -d "Docker volume"
     end
 end
 
-function gcom -d "git commit with chatgpt"
+function gt -d "git commit with chatgpt"
     git commit -am "$(dime -i "Eres un fantástico desarrollador, especializado en gestionar proyectos con control de versiones" -q "genera un mensaje conciso para un `git commit` que resuma los cambios producidos en ```$(git -P diff)```. Solo me tienes que dar el mensaje, ni la descripción, ni una explicación. Únicamente un mensaje en español, que comience con un gitmoji y sin comillas")"
 end
 
-function gpush -d "git push"
+function gh -d "git push"
     git push
 end
 
-function gadd -d "git add"
+function gd -d "git add"
     git add $argv
 end
 
-function gpull -d "git pull"
+function gl -d "git pull"
     git pull
 end
 
-function gstat -d "git status"
+function gs -d "git status"
     git status
 end
 
-function gmerge -d "git merge --no-edit --no-ff development"
+function gm -d "git merge --no-edit --no-ff development"
     git switch main
     git merge --no-edit --no-ff development
     git push
