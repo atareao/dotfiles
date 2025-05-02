f489e3832fbce8d40af432a780178988a9f657bd
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2025-05-02 07:09:03 +0200

    ✏️ Renombradas y modificadas funciones de alias en Fish; eliminado `init.lua` de Yazi.

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -18,27 +18,31 @@ function dv -d "Docker volume"
     end
 end
 
-function gt -d "git commit with chatgpt"
+function ycom -d "yadm git commit with chatgpt"
+    yadm commit -am "$(dime -i "Eres un fantástico desarrollador, especializado en gestionar proyectos con control de versiones" -q "genera un mensaje conciso para un `git commit` que resuma los cambios producidos en ```$(yadm -P diff)```. Solo me tienes que dar el mensaje, ni la descripción, ni una explicación. Únicamente un mensaje en español, que comience con un gitmoji y sin comillas")"
+end
+
+function gcom -d "git commit with chatgpt"
     git commit -am "$(dime -i "Eres un fantástico desarrollador, especializado en gestionar proyectos con control de versiones" -q "genera un mensaje conciso para un `git commit` que resuma los cambios producidos en ```$(git -P diff)```. Solo me tienes que dar el mensaje, ni la descripción, ni una explicación. Únicamente un mensaje en español, que comience con un gitmoji y sin comillas")"
 end
 
-function gh -d "git push"
+function gpus -d "git push"
     git push
 end
 
-function gd -d "git add"
+function gadd -d "git add"
     git add $argv
 end
 
-function gl -d "git pull"
+function gpul -d "git pull"
     git pull
 end
 
-function gs -d "git status"
+function gsta -d "git status"
     git status
 end
 
-function gm -d "git merge --no-edit --no-ff development"
+function gmer -d "git merge --no-edit --no-ff development"
     git switch main
     git merge --no-edit --no-ff development
     git push
