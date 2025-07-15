464385470cdf0132f7b2a2475c645fdb17a3a6a7
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2025-07-15 07:03:58 +0200

    🎨 Añade estilos responsive y mejora la maquetación

---
+++ .config/nvim/templates/podcast.md
@@ -0,0 +1,22 @@
+---
+title: {{_cursor_}}
+season: 
+episode: 
+created: {{_lua:os.date("%y-%m-%dT%H:%M")_}}
+updated: {{_lua:os.date("%y-%m-%dT%H:%M")_}}
+tags:
+  - 
+---
+# 
+
+
+
+<!--more-->
+##
+
+
+
+---
+Más información,
+
+- 
--- .config/nvim/templates/template.sh
+++ .config/nvim/templates/template.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-# Copyright (c) {{YEAR}} {{NAME}}
+# Copyright (c) {{_lua:os.date("%y")_}} {{_author_}}
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
