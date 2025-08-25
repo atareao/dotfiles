03c9710ad9a1e2e094e6f553aafa6e85b425b52d
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2025-08-25 10:40:53 +0200

    🚀 Añade funcionalidad de autenticación de usuarios y mejora la interfaz de inicio

--- .config/nvim/lsp/ruff.lua
+++ .config/nvim/lsp/ruff.lua
@@ -1,8 +1,39 @@
 return {
-  init_options = {
-    -- the settings can be found here: https://docs.astral.sh/ruff/editors/settings/
-    settings = {
-      organizeImports = true,
+    default_config = {
+        cmd = { 'ruff', 'server' },
+        filetypes = { 'python' },
+        root_markers = { 'pyproject.toml', 'ruff.toml', '.ruff.toml', '.git' },
+        single_file_support = true,
+        settings = {},
     },
-  },
+    docs = {
+        description = [[
+https://github.com/astral-sh/ruff
+
+A Language Server Protocol implementation for Ruff, an extremely fast Python linter and code formatter, written in Rust. It can be installed via `pip`.
+
+```sh
+pip install ruff
+```
+
+**Available in Ruff `v0.4.5` in beta and stabilized in Ruff `v0.5.3`.**
+
+This is the new built-in language server written in Rust. It supports the same feature set as `ruff-lsp`, but with superior performance and no installation required. Note that the `ruff-lsp` server will continue to be maintained until further notice.
+
+Server settings can be provided via:
+
+```lua
+require('lspconfig').ruff.setup({
+  init_options = {
+    settings = {
+      -- Server settings should go here
+    }
+  }
+})
+```
+
+Refer to the [documentation](https://docs.astral.sh/ruff/editors/) for more details.
+
+  ]],
+    },
 }
