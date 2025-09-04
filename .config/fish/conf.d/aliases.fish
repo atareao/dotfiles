d0c950ef4da5e096b03f007c4ae10337e37f6d34
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2025-09-04 20:21:10 +0200

    feat: Añadidas funciones para push, pull, add y status en YADM

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -22,6 +22,23 @@ function ycom -d "yadm git commit with chatgpt"
     yadm commit -am "$(dime -i "Eres un fantástico desarrollador, especializado en gestionar proyectos con control de versiones" -q "genera un mensaje conciso para un `git commit` que resuma los cambios producidos en ```$(yadm -P diff)```. Solo me tienes que dar el mensaje, ni la descripción, ni una explicación. Únicamente un mensaje en español, que comience con un gitmoji y sin comillas")"
 end
 
+function ypus -d "yadm push"
+    yadm push
+end
+
+function yadd -d "yadm add"
+    yadm add $argv
+end
+
+function ypul -d "yadm pull"
+    yadm pull
+end
+
+function ysta -d "yadm status"
+    yadm status
+end
+
+
 function gcom -d "git commit with chatgpt"
     git commit -am "$(dime -i "Eres un fantástico desarrollador, especializado en gestionar proyectos con control de versiones" -q "genera un mensaje conciso para un `git commit` que resuma los cambios producidos en ```$(git -P diff)```. Solo me tienes que dar el mensaje, ni la descripción, ni una explicación. Únicamente un mensaje en español, que comience con un gitmoji y sin comillas")"
 end
