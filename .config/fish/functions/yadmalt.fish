c94d3ecca105682de38ff280172ba0400adf8dae
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2026-01-10 09:38:51 +0100

    🚀 Añade nueva funcionalidad y corrige errores en el módulo de autenticación

--- .config/fish/functions/yadmalt.fish
+++ .config/fish/functions/yadmalt.fish
@@ -1,14 +1,19 @@
-function yadmalt --description="generate templates with gopass variables"
+function yadmalt --description="generate templates with pass variables"
     # set variables
-    gopass list --flat | while read -l item
+    set -l store (set -q PASSWORD_STORE_DIR; and echo $PASSWORD_STORE_DIR; or echo $HOME/.password-store)
+    if not test -d $store
+        echo "Error: No se encontró el almacén de contraseñas en $store" >&2
+        return 1
+    end
+    fd --extension gpg --base-directory $store | string replace -r '\.gpg$' '' | while read -l item
         set variable $(string replace \/ _ "$item" | string upper)
         echo $variable
-        set -xg $variable "$(gopass show --password $item)"
+        set -xg $variable "$(pass show $item)"
     end
     # configure templates
     yadm alt
     # unset variables
-    gopass list --flat | while read -l item
+    fd --extension gpg --base-directory $store | string replace -r '\.gpg$' '' | while read -l item
         set variable $(string replace \/ _ "$item" | string upper)
         set -e "$variable"
     end
