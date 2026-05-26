bb72e1410977bbcf741199c86251c0032f8da448
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2026-05-26 18:56:25 +0200

    🤦‍♂️ Intentando no quemar el código.

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -3,7 +3,7 @@ set -U MY_ABBR_SET true
 abbr --add n nvim
 abbr --add vimd nvim -d
 abbr --add play ansible-playbook
-abbr --add dlm 'yt-dlp --ignore-errors --format bestaudio --extract-audio --audio-format mp3 --audio-quality 160K --output "%(title)s.%(ext)s" --yes-playlist' 
+abbr --add dlm 'yt-dlp --ignore-errors --format bestaudio --extract-audio --audio-format mp3 --audio-quality 160K --output "%(title)s.%(ext)s" --yes-playlist'
 # kitty
 abbr --add shs kitty +kitten ssh -q
 abbr --add icat kitty +kitten icat
@@ -13,6 +13,7 @@ abbr --add ls lsd
 abbr --add la lsd -la
 abbr --add lt lsd --tree
 abbr --add du dust
+abbr --add hx helix
 # trasher
 abbr --add tls trasher --trash-dir ~/.trashdir ls
 abbr --add trm trasher --trash-dir ~/.trashdir rm
